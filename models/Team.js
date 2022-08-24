2876e3c83427543a13774f97091790c1796b7c39
Britt Carlton <brittanyrc95@gmail.com>
2022-08-23 20:54:35 -0400

    Create team and member models

---
+++ models/Team.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const { User } = require('./User');
+
+class Team extends Model {}
+
+Team.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: User,
+                key: 'id'
+            }
+        },
+        team_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'team'
+    }
+);
+
+module.exports = Team;
