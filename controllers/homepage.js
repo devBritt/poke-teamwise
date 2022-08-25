b89779b2911f85be37db03fca3dfe63db16c5757
KyleAxley <kanderson7645@yahoo.com>
2022-08-25 19:05:34 -0400

    user model created

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -1,37 +1,35 @@
 const router = require("express").Router();
 
-router.get ('/', (req, res) => {
-    if(req.session.loggedIn)
-    res.redirect('/dashboard')
-    else res.sendFile('homepage.html', {root:path.join(__dirname, 'public')})
-})
+router.get("/", (req, res) => {
+  if (req.session.loggedIn) res.redirect("/dashboard");
+  else res.sendFile("homepage.html", { root: path.join(__dirname, "public") });
+});
 
-router.get('/dashboard', (req, res) => {
-    if (req.session.loggedIn) {
-        res.setHeader('Content-Type', 'text/html')
-        res.write ('Welcome ' + req.session.username+' to your dashboard')
-        res.write('<a href="/logout">Logout</a>')
-        res.end ()
-    }
-    else res.redirect('/login')
-})
+router.get("/dashboard", (req, res) => {
+  if (req.session.loggedIn) {
+    res.setHeader("Content-Type", "text/html");
+    res.write("Welcome " + req.session.username + " to your dashboard");
+    res.write('<a href="/logout">Logout</a>');
+    res.end();
+  } else res.redirect("/login");
+});
 
-router.get ('/login', (req, res) => {
-    res.sendFile('login.html',{root:path.join(__dirname,'public')})
-})
+router.get("/login", (req, res) => {
+  res.sendFile("login.html", { root: path.join(__dirname, "public") });
+});
 
-router.post('/authenticate'
-,(req,res,next) => {
-    if (req.body.username == 'foo'&&req.body.password=='bar') 
-    {
-        res.locals.username = req.body.username
-        next ()
-    }
-    else res.sendStatus (401)
-    }
-    , (req, res) => {
-        req.session.destroy((err) => {})
-        res.send ('Thank you!')
-    })
+router.post(
+  "/authenticate",
+  (req, res, next) => {
+    if (req.body.username == "foo" && req.body.password == "bar") {
+      res.locals.username = req.body.username;
+      next();
+    } else res.sendStatus(401);
+  },
+  (req, res) => {
+    req.session.destroy((err) => {});
+    res.send("Thank you!");
+  }
+);
 
-    module.exports = router;
+module.exports = router;
