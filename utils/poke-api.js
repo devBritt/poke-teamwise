4b07789ccd97de1a379a4cd0f7d25376d539df35
Britt Carlton <brittanyrc95@gmail.com>
2022-08-26 11:53:49 -0400

    Explore PokeAPI responses

--- utils/poke-api.js
+++ utils/poke-api.js
@@ -6,13 +6,6 @@ const games = require('./poke-games');
 // base pokedex URL
 const baseDexUrl = 'https://pokeapi.co/api/v2/pokedex/'
 
-// TODO: get list of all Pokémon based on selected game
-async function getDexEntries(dexId) {
-    const response = await axios.get(baseDexUrl + dexId);
-
-    console.log(response.data);
-}
-
 // TODO: get list of Pokémon stats
 async function getPokeDetails(pokemon) {
     const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`);
@@ -20,18 +13,46 @@ async function getPokeDetails(pokemon) {
     console.log(response.data.stats);
 }
 
-// TODO: get Pokémon by type
-async function getPokeByType(type) {
-    const response = await axios.get(`https://pokeapi.co/api/v2/type/${type}`);
+// get list of all pokemon from chosen game (by pokedex id)
+async function getDexEntries(dexId) {
+    // array to store pokemon_entries as objects
+    const dexArr = [];
 
-    console.log(response.data.pokemon);
+    // get dex by game
+    const response = await axios.get(baseDexUrl + dexId);
+
+    response.data.pokemon_entries.map(element => {
+        dexArr.push(element);
+    });
+
+    return dexArr;
 }
 
-// TODO: get Pokémon by move
+// request list of all pokemon with chosen type
+async function getPokeByType(type) {
+    const pokeByTypeArr = [];
+    const response = await axios.get(`https://pokeapi.co/api/v2/type/${type}`);
+    
+    response.data.pokemon.map(element => {
+        pokeByTypeArr.push(element.pokemon);
+    });
+
+    return pokeByTypeArr;
+}
+
+// request list of all pokemon that can learn chosen move
 async function getPokeByMove(move) {
+    // array to store learned_by_pokemon as objects
+    const pokeByMoveArr = [];
+    
+    // get pokemon by move
     const response = await axios.get(`https://pokeapi.co/api/v2/move/${move}`);
 
-    console.log(response.data.learned_by_pokemon);
+    response.data.learned_by_pokemon.map(element => {
+        pokeByMoveArr.push(element);
+    });
+
+    return pokeByMoveArr;
 }
 
 // TODO: get Pokémon by type and move
@@ -42,9 +63,3 @@ async function getPokeFiltered(game, type, move) {
 
 
 }
-
-getDexEntries(games[games.length - 1].dexId);
-// getPokedex(3);
-// getPokeDetails('trubbish');
-// getPokeByType('fire');
-// getPokeByMove('solar-beam');
