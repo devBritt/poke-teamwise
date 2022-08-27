b5a35c1955d41f76ce0c292134170d45aea91081
Britt Carlton <brittanyrc95@gmail.com>
2022-08-27 12:35:38 -0400

    Create generator-page-routes

---
+++ controllers/api/member-routes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const { User, Member } = require('../../models'); 
+
+// GET members by team_id
+router.get('/', (req, res) => {
+    
+});
+
+// GET member by id
+router.get('/:id', (req, res) => {
+
+});
+
+// POST new member
+router.post('/', (req, res) => {
+
+});
+
+// PUT update member by id
+router.put('/:id', (req, res) => {
+
+});
+
+// DELETE member by id
+router.delete('/:id', (req, res) => {
+
+});
+
+
+module.exports = router;
+
