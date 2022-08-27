f5c1078420b4ff6794d46f62680f3ae2592fd555
KyleAxley <kanderson7645@yahoo.com>
2022-08-27 13:06:19 -0400

    user routes fnished

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,12 +1,75 @@
 const router = require("express").Router();
 const { User, Favorites, Team } = require("../../models");
 
-//TODO ⬇️
-router.get("/", (req, res) => {});
-//TODO ⬇️
-router.get("/:id", (req, res) => {});
-//TODO ⬇️
-router.post("/", (req, res) => {});
+//route to find all users
+router.get("/", (req, res) => {
+  User.findAll({
+    attributes: { exclude: ["password"]},
+  })
+  .then((dbUserData) => res.json(dbUserData))
+  .catch((err) =>{
+    console.log(err);
+    res.status(500).json(err);
+  })
+});
+
+//route to find a single user by id
+router.get("/:id", (req, res) => {
+  User.findOne({
+    attributes: { exclude: ["password"]},
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Favorites,
+        attributes: ['id', 'poke_id']
+      },
+      {
+        model: Team,
+        attributes: ['id', 'team_name'],
+        include: {
+          model: Favorites,
+          attributes: ['favorites_id']
+        }
+      }
+    ]
+  })
+  .then((dbUserData) => {
+    if(!dbUserData) {
+      res.status(404).json({message: "There is no user found by this id"});
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  })
+});
+
+//route to create new user
+router.post("/", (req, res) => {
+  //expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+  .then((dbUserData) => {
+    req.session.loggedIn = true;
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+
+      res.json(dbUserData);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
 
 //route to log in user
 router.post("/login", (req, res) => {
@@ -50,8 +113,26 @@ router.post("/logout", (req, res) => {
   }
 });
 
-//TODO ⬇️
-router.put("/:id", (req, res) => {});
+//route to update a single user
+router.put("/:id", (req, res) => {
+  User.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((dbUserData) => {
+    if(!dbUserData) {
+      res.status(404).json({ message: "There is no user found by this id!"});
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
 
 //route to delete single user
 router.delete("/:id", (req, res) => {
