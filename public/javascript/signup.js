66b781535ac55c1b47c4908aaacd10f224d8137f
KyleAxley <kanderson7645@yahoo.com>
2022-08-28 13:27:26 -0400

    delete js added

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,28 +1,30 @@
 async function signupFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const username = document.querySelector("#username-signup").value.trim();
-    const email = document.querySelector("#email-signup").value.trim();
-    const password = document.querySelector("#password-signup").value.trim();
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
 
-    if(username && email && password) {
-        const response = await fetch('/api/user', {
-            method: 'post',
-            body: JSON.stringify({
-                username,
-                email,
-                password,
-            }),
-            headers: {"Content-Type": "application/json"},
-        });
+  if (username && email && password) {
+    const response = await fetch("/api/user", {
+      method: "post",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-        if(response.ok) {
-            document.location.replace("/dashboard/");
-        } else {
-            alert(response.statusText);
-        }
+    if (response.ok) {
+      document.location.replace("/dashboard/");
+    } else {
+      alert(response.statusText);
     }
-    console.log("confirmed click");
+  }
+  console.log("confirmed click");
 }
 
-document.querySelector('.signup-form').addEventListener("submit", signupFormHandler);
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
