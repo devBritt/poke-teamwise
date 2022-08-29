700a9c5b201ca2810f3efa9d71cb8964d28b0619
KyleAxley <kanderson7645@yahoo.com>
2022-08-29 15:08:55 -0400

    dashboard edit, working on styling

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -7,37 +7,10 @@ const withAuth = require("../utils/auth");
 
 
 router.get("/", (req, res) => {
-//   Favorites.findAll({
-//     where: {
-//       user_id: req.session.user_id,
-//     },
-//     attributes: ["id", "poke_id"],
-//     include: [
-//       {
-//         model: Team,
-//         attributes: ["id", "team_name", "poke_id"],
-//         include: {
-//           model: User,
-//           attributes: ["username"],
-//         },
-//       },
-//       {
-//         model: User,
-//         attributes: ["username"],
-//       },
-//     ],
-//   })
-//     .then((dbTeamData) => {
-//       const teams = dbTeamData.map((team) => team.get({ plain: true }));
-//       console.log(teams);
-//       res.render("dashboard", { teams, loggedIn: true });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
+
     res.render('dashboard', { 
-        games, loggedIn: true
+        loggedIn: req.session.loggedIn,
+        games,
     });
 });
 
